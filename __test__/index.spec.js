bda054d31d14141e4d962a50c9819c24608c2ef4
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-10-04 18:21:37 -0400

    integration test are added

---
+++ __test__/index.spec.js
@@ -0,0 +1,17 @@
+const app = require('../server');
+const request = require("supertest");
+//const sequelize = require('../config/dbSetup');
+
+
+describe("GET /healthz ", () => {
+  test("It should respond 200", async () => {
+    //expect.assertions(1); 
+    const response = await request(app).get("/healthz");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+/* Closing the database connection after all tests have run
+afterAll(async () => {
+  await sequelize.close(); // This ensures that the DB connection is closed after tests
+});*/
