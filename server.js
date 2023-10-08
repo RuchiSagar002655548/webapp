3eeb078fc1d2e2cab0f53d9de7332fa0344b9c88
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-10-07 20:12:25 -0400

    update yml

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ db.user.hasMany(db.assignment, {foreignKey: "owner_user_id"});
 db.sequelize.sync({force: false})
   .then(() =>{
 
-   console.log("Database setup complete.");
+   //console.log("Database setup complete.");
    
   // Call the newUser function to process and insert the CSV data
    newUser({}, {                   // Passing an empty req object and defining res object
