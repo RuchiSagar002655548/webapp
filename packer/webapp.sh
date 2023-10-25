2a2824beef5427cd6bf878fe14a4cce92ae8a8f8
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-10-25 15:07:00 -0400

    ec2 user added

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -19,11 +19,26 @@ unzip webapp.zip -d webapp
 cd webapp
 npm install
 
-echo "Setting up and starting the webapp service"
+# Creating group and user for running the webapp
+echo "Creating group and adding ec2 user"
+sudo groupadd ec2-user
+sudo useradd -s /bin/false -g ec2-user ec2-user
+
+# Give ownership of the webapp directory to ec2-user
+echo "Changing ownership of the webapp directory"
+sudo chown -R ec2-user:ec2-user /home/admin/webapp
+
+# Copy the systemd service file
+echo "Setting up the webapp service"
 sudo cp /home/admin/webapp/packer/webapp.service /etc/systemd/system
+
+# Change ownership of the service file 
+sudo chown ec2-user:ec2-user /etc/systemd/system/webapp.service
+
+# Start the service
+echo "Starting the webapp service"
 sudo systemctl daemon-reload
 sudo systemctl enable webapp
 sudo systemctl start webapp
-sudo systemctl restart webapp
- 
+
 echo "Script executed successfully!"
