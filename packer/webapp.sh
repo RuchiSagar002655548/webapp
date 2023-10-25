0cb10ce2b4f599f73684c8850c8c52758840e008
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-10-25 00:42:40 -0400

    webapp.sh file update

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -1,41 +1,15 @@
 #!/bin/bash
+set -e  # This will cause the script to exit if any command returns a non-zero exit code
 
 # Update the system
 sudo apt update -y
 sudo apt upgrade -y
 
-# Export database credentials and other env variables
-export DBHOST="127.0.0.1"
-export DBUSER="root"
-export DBPASS="Root123#"
-export DATABASE="assign3_db"
-export PORT=3000
-export DBPORT=3306
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Install MariaDB
-sudo apt-get install -y mariadb-server
-sudo systemctl start mariadb
-
-
-# Log in as superuser
-sudo mysql -u root <<EOF
--- Change the authentication method to mysql_native_password
-ALTER USER '$DBUSER'@'$DBHOST' IDENTIFIED WITH 'mysql_native_password' BY '$DBPASS';
-FLUSH PRIVILEGES;
-
--- Create the database and user
-CREATE DATABASE IF NOT EXISTS $DATABASE;
-GRANT ALL PRIVILEGES ON $DATABASE.* TO '$DBUSER'@'$DBHOST' IDENTIFIED BY '$DBPASS' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
-echo "MySQL setup completed. Database '$DATABASE' and user '$DBUSER' on host '$DBHOST' have been created.
-#sudo mysqladmin -u ${DBUSER} password ${DBPASS}
-#mysqladmin -u ${DBUSER} --password=${DBPASS} --host=${DBHOST} --port=${DBPORT} create ${DATABASE}
-sudo systemctl enable mariadb
 
 # Install unzip if not installed
 sudo apt-get install unzip -y
@@ -44,3 +18,12 @@ sudo apt-get install unzip -y
 unzip webapp.zip -d webapp
 cd webapp
 npm install
+
+echo "Setting up and starting the webapp service"
+sudo cp /home/admin/webapp/packer/webapp.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp
+sudo systemctl restart webapp
+ 
+echo "Script executed successfully!"
