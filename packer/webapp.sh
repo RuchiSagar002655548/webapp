8b0b8cdea03307b1425fba29aea3d9240a11e952
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-10-25 20:11:01 -0400

    updating sh file

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -2,43 +2,37 @@
 set -e  # This will cause the script to exit if any command returns a non-zero exit code
 
 # Update the system
+echo "Updating the system"
 sudo apt update -y
 sudo apt upgrade -y
 
-
 # Install Node.js
+echo "Installing Node.js"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-
 # Install unzip if not installed
+echo "Installing unzip"
 sudo apt-get install unzip -y
 
-# This will unzip the web application
+# Unzip the web application
+echo "Unzipping the web application"
 unzip webapp.zip -d webapp
+
+# Navigate to the webapp directory and install node modules
+echo "Installing node modules"
 cd webapp
 npm install
 
-# Creating group and user for running the webapp
-echo "Creating group and adding ec2 user"
-sudo groupadd ec2-user
-sudo useradd -s /bin/false -g ec2-user ec2-user
-
-# Give ownership of the webapp directory to ec2-user
-echo "Changing ownership of the webapp directory"
-sudo chown -R ec2-user:ec2-user /home/admin/webapp
-
-# Copy the systemd service file
-echo "Setting up the webapp service"
+# Copy the systemd service file and start the service
+echo "Setting up and starting the webapp service"
 sudo cp /home/admin/webapp/packer/webapp.service /etc/systemd/system
-
-# Change ownership of the service file 
-sudo chown ec2-user:ec2-user /etc/systemd/system/webapp.service
-
-# Start the service
-echo "Starting the webapp service"
 sudo systemctl daemon-reload
-sudo systemctl enable webapp
-sudo systemctl start webapp
+sudo systemctl start webapp.service
+sudo systemctl enable webapp.service
+
 
 echo "Script executed successfully!"
+
+
+
