47e8603b65a5ff54c2035dd3e07d4e65827defeb
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-11-28 08:48:13 -0800

    assignmentroute updated

--- api-routes/assignmentRoutes.js
+++ api-routes/assignmentRoutes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+ 
 var assignment = require('../services/assignments');
+var submission = require('../services/submission');
 const logger = require('../logger/loggerindex');
 var helper = require('../config/helper');
-
+ 
 // Middleware to check for query parameters
 router.use((req, res, next) => {
     if (Object.keys(req.query).length !== 0) {
@@ -12,28 +13,39 @@ router.use((req, res, next) => {
     }
     next();
 });
-
+ 
 router.post('/', helper.pAuthCheck, assignment.createNewAssignment);
-
+ 
 router.get('/', helper.pAuthCheck, assignment.getAssignmentList);
-
+ 
 router.get('/:id', helper.pAuthCheck, assignment.getAssignmentDetails);
-
+ 
 router.put('/:id', helper.pAuthCheck, assignment.putAssignmentDetails);
-
+ 
 router.delete('/:id', helper.pAuthCheck, assignment.deleteAssignment);
-
-
+ 
+router.post('/:id/submission', helper.pAuthCheck, submission.createNewSubmission); //added
+ 
 // Add this to return 405 for PATCH requests
 router.patch('/', (req, res) => {
     logger.error({uri: "/v1/assignments", statusCode: 405, message: "Method Not Allowed"});
     res.status(405).set('Cache-Control', 'no-store, no-cache, must-revalidate').send();
 });
-
+ 
 // Add this to return 405 for PATCH requests
 router.patch('/:id', (req, res) => {
     logger.error({uri: "/v1/assignments" + req.params.id, statusCode: 405, message: "Method Not Allowed"});
     res.status(405).set('Cache-Control', 'no-store, no-cache, must-revalidate').send();
 });
-
+ 
+router.all('/:id/submission', (req, res) => {
+    logger.error({uri: "/v1/assignments" + req.params.id + "/submission", statusCode: 405, message: "Method Not Allowed"});
+    res.status(405).set('Cache-Control', 'no-store, no-cache, must-revalidate').send();
+});
+ 
+router.use('/:id/:wrongPath', (req, res) => {
+    logger.error({uri: "/v1/assignments" + req.params.id + "/submission", statusCode: 400, message: "Enter correct path"});
+    res.status(400).set('Cache-Control', 'no-store, no-cache, must-revalidate').send();
+});
+ 
 module.exports = router;
