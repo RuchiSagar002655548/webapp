851c6b4aa2140e0afd4dca82d410700b35920f29
Ruchi Anand Sagar <sagar.ru@northeastern.edu>
2023-12-04 15:39:45 -0500

    end point updated

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use('/healthz', (req, res) => {
 });
  
  
-app.use('/v2/assignments', assignmentRoutes);   //v2
+app.use('/v2/assignments', assignmentRoutes);   //v2 end point
  
 app.use((req, res) => {
   res.status(404).send();
